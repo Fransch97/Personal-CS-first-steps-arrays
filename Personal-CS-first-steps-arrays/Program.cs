a46a763ccadbcf7a7e466e57c3f83e8272b38078
fransch97 <fransch345@gmail.com>
2022-07-17 01:31:38 +0200

    Aggiungere i file di progetto.

---
+++ Personal-CS-first-steps-arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //array normali
+            string[] primo = { "uno", "due" , "tre", "quattro" };
+            foreach (string x in primo)
+            {
+                Console.WriteLine(x + "\n");
+            }
+
+            int[] secondo = {1 , 2, 3}; 
+            foreach (int x in secondo)
+            {
+                Console.WriteLine (x + "\n");
+            }
+
+            //array multidimensionali
+
+            //2D
+            string[,] array2d =
+            {
+                { "aaaa", "bbb" },
+                { "ccc", "ddd" },
+                { "eee", "fff" }
+            };
+
+            foreach(string x in array2d)
+            {
+                    Console.WriteLine((string)x);
+            };
+
+
+
+            //3D
+            string[,,] array3D =
+            {
+                {   
+                    {"xxx", "yyyy","eee" }
+                },
+            };
+
+            foreach (string x in array3D)
+            {
+                Console.WriteLine ((string)x);
+            }
+
+
+
+
+
+        }
+    }
+}
